cd2bf4fe44b3e2c7aaafd6cd78aa8001eeec5bd0
snow.xiao <180205585@qq.com>
2017-01-17 18:53:06 +0800

    add UIManager

--- Assets/Scripts/FrameWork/Manager/UIManager.cs
+++ Assets/Scripts/FrameWork/Manager/UIManager.cs
@@ -6,11 +6,25 @@ namespace SnowFrameWork
 { 
 	public class UIManager : Singleton<UIManager>  
 	{
+		/// <summary>
+		/// User interface info data.
+		/// </summary>
 		class UIInfoData
 		{
+			/// <summary>
+			/// Gets the type of the user interface.
+			/// </summary>
+			/// <value>The type of the user interface.</value>
 			public EnumUIType UIType{ get; private set; }
 			public string Path{ get; private set; }
+			public object[]  UIparams{ get; private set; }
 
+			UIInfoData( EnumUIType _uiType, string _path, params object[] _uiParams )
+			{
+				UIType = _uiType;
+				Path = _path;
+				UIparams = _uiParams;
+			}
 		}
 		/// <summary>
 		/// The dic open U is. 当前打开UI的集合
@@ -20,7 +34,7 @@ namespace SnowFrameWork
 		/// <summary>
 		/// The stack open U is. 将要打开的UI
 		/// </summary>
-		private Stack<EnumUIType> stackOpenUIs = null;
+		private Stack<UIInfoData> stackOpenUIs = null;
 
 		/// <summary>
 		/// Init this instance.
@@ -29,8 +43,34 @@ namespace SnowFrameWork
 		/// </summary>
 		public override void Init()
 		{
-			base.Init ();
-			Debug.Log ("UIManager : Singleton<UIManager> Init");
+			dicOpenUIs = new Dictionary< EnumUIType, GameObject> ();
+			stackOpenUIs = new Stack<UIInfoData> ();
+		}
+			
+		public T GetUI<T>( EnumUIType _uiType) where T : BaseUI
+		{
+			GameObject _retObj = GetUIObject (_uiType); 
+			if (_retObj != null)
+				return _retObj.GetComponent<T> ();
+			return null;
+		}
+
+		/// <summary>
+		/// Gets the user interface object.
+		/// </summary>
+		/// <returns>The user interface object.</returns>
+		/// <param name="_uiType">User interface type.</param>
+		public GameObject GetUIObject (EnumUIType _uiType)
+		{
+			GameObject _retObj = null;
+			if (!dicOpenUIs.TryGetValue (_uiType, _retObj))
+				throw new Exception ("_dicOpenUIs TryGetValue Failure! _uiType: " + _uiType.ToString);
+			return _retObj;
+		}
+
+		public void OpenUI ( bool _isCloseOhter, EnumUIType[] _uiTypes, params ) 
+		{
+			
 		}
     }
 }
